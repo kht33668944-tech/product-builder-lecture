2ca9874128cb1a27a4e2fe6bc1e57e682a8157e6
kht33668944 <kht33668944@gmail.com>
2026-02-03 04:28:32 +0000

    feat: add dark mode/white mode toggle

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const numberDisplay = document.querySelector('.number-display');
 const generateBtn = document.getElementById('generate-btn');
 const historyList = document.getElementById('history-list');
+const themeSwitch = document.getElementById('checkbox');
 
 const generateNumbers = () => {
     const numbers = new Set();
@@ -28,3 +29,26 @@ generateBtn.addEventListener('click', () => {
     displayNumbers(newNumbers);
     addToHistory(newNumbers);
 });
+
+// Theme switching
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        themeSwitch.checked = true;
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
+}
+
+themeSwitch.addEventListener('change', switchTheme, false);
